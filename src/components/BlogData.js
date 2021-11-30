7c9627f30c2fcda1a81a924aed0bbf9cf104fca7
Haidar1528 <haidaralimasuhaidarali@gmail.com>
2021-11-30 14:08:02 +0530

    initial commit

---
+++ src/components/BlogData.js
@@ -0,0 +1,27 @@
+export const Blogs = [
+    {
+        id:1,
+        name:"What is Web3.0 ? And Why we need it ?",
+        tags:["blockchain","Web3","Internet"],
+        date:"1 Oct, 2021",
+        imgSrc:"https://blog.kodinghandle.com/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1633058171811%2F3EiwXYVKv.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+        link:"https://blog.kodinghandle.com/what-is-web30-and-why-we-need-it"
+    },
+    {
+        id:2,
+        name:"What is BlockChian ? Detailed expaination.",
+        tags:["blockchain","bitcoin", "ethereum"],
+        date:"5 July, 2021",
+        imgSrc:"https://blog.kodinghandle.com/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1633060464276%2FMIX9igL3V.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+        link:"https://blog.kodinghandle.com/what-is-blockchian-detailed-expaination"
+    },
+    {
+        id:3,
+        name:"What is React & Why we need it ?",
+        tags:["react","javascript", ],
+        date:"22 July, 2021",
+        imgSrc:"https://cdn.hashnode.com/res/hashnode/image/upload/v1633058197394/t0cCE-BFxG.png?auto=compress,format&format=webp",
+        link:"https://blog.kodinghandle.com/what-is-blockchian-detailed-expaination"
+    }
+  
+] 
---
+++ src/components/WorkData.js
@@ -0,0 +1,42 @@
+export const Work = [
+    {
+        id:1,
+        name:"NFT Marketplace",
+        description:"Fullstack NFT marketplace on top of Polygon powered by smart contracts.",
+        tags:["react","eth-brownie","polygon","smart contracts"],
+        
+        demo:"https://www.bullcarter.in",
+        github:"https://github.com/haidaralimasu/react-non-fungible-mall"
+    },
+    {
+        id:2,
+        name:"NFT minting DApp",
+        description:"A NFT minting DApp on ethereum allows you to mint Iskai NFT.",
+        tags:["nft","ethereum", "blockchain"],
+        
+        demo:"https://prestigelab.vercel.app",
+        github:"https://github.com/haidaralimasu/option-swap"
+        
+    },
+    {
+        id:3,
+        name:"Defi stacking App",
+        description:"A Defi protocol for user to earn rewards by stacking their coins.",
+        tags:["eth-brownie","solidity","Web3"],
+        
+        demo:"https://github.com/haidaralimasu/brownie-react-defi",
+        github:"https://github.com/haidaralimasu/brownie-react-defi"
+    },
+    {
+        id:4,
+        name:"Ethereum Wallet",
+        description:"An Ethereum Wallet allow user to send eth across diffrent account.",
+        tags:["usedapp","solidity","Web3"],
+        
+        demo:"https://eth-wallet-v2.vercel.app/",
+        github:"https://github.com/haidaralimasu/eth-wallet-v2"
+    },
+
+    
+  
+] 
