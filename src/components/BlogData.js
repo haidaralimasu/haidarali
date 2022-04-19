ba2be741b7178e6c3a296751a2a1b79b8bc55678
Haidarali Masu <haidaralimasu123@gmail.com>
2022-04-19 17:57:21 +0530

    Update BlogData.js

--- src/components/BlogData.js
+++ src/components/BlogData.js
@@ -4,16 +4,16 @@ export const Blogs = [
         name:"What is Web3.0 ? And Why we need it ?",
         tags:["blockchain","Web3","Internet"],
         date:"1 Oct, 2021",
-        imgSrc:"https://blog.kodinghandle.com/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1633058171811%2F3EiwXYVKv.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-        link:"https://blog.kodinghandle.com/what-is-web30-and-why-we-need-it"
+        imgSrc:"https://blog.haidaralimasu.com/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1633058171811%2F3EiwXYVKv.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+        link:"https://blog.haidaralimasu.com/what-is-web30-and-why-we-need-it"
     },
     {
         id:2,
         name:"What is BlockChian ? Detailed expaination.",
         tags:["blockchain","bitcoin", "ethereum"],
         date:"5 July, 2021",
-        imgSrc:"https://blog.kodinghandle.com/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1633060464276%2FMIX9igL3V.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-        link:"https://blog.kodinghandle.com/what-is-blockchian-detailed-expaination"
+        imgSrc:"https://blog.haidaralimasu.com/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1633060464276%2FMIX9igL3V.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+        link:"https://blog.haidaralimasu.com/what-is-blockchian-detailed-expaination"
     },
     {
         id:3,
@@ -21,15 +21,15 @@ export const Blogs = [
         tags:["react","javascript", ],
         date:"22 July, 2021",
         imgSrc:"https://cdn.hashnode.com/res/hashnode/image/upload/v1633058197394/t0cCE-BFxG.png?auto=compress,format&format=webp",
-        link:"https://blog.kodinghandle.com/what-is-blockchian-detailed-expaination"
+        link:"https://blog.haidaralimasu.com/what-is-blockchian-detailed-expaination"
     },
     {
         id: 4,
         name:"What is Ethereum ? How to get started with it ?",
         tags:["blockchain","reactjs", "ethereum" ],
         date:"17 December, 2021",
-        imgSrc:"https://blog.kodinghandle.com/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1639751271895%2FOKk7QCDEa.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=3840&q=75",
-        link:"https://blog.kodinghandle.com/what-is-ethereum-how-to-get-started-with-it"
+        imgSrc:"https://blog.haidaralimasu.com/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1639751271895%2FOKk7QCDEa.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=3840&q=75",
+        link:"https://blog.haidaralimasu.com/what-is-ethereum-how-to-get-started-with-it"
     },
     {
         id: 5,
@@ -37,7 +37,7 @@ export const Blogs = [
         tags:["apis", "graphql" ],
         date:"17 December, 2021",
         imgSrc:"https://cdn.hashnode.com/res/hashnode/image/upload/v1633058770508/hYDEwbz0-.png?auto=compress,format&format=webp",
-        link:"https://blog.kodinghandle.com/graphql"
+        link:"https://blog.haidaralimasu.com/graphql"
     }
   
 ] 
